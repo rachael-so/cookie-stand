1958320b057cfd884c04cc870c9c952d7776f542
rachael-so <rachaelso@gmail.com>
2017-06-15 12:31:30 -0700

    stretch goals from other days

--- app.js
+++ app.js
@@ -52,21 +52,6 @@ Store.prototype.render = function() {
   theTable.appendChild(trEl);
 };
 
-function renderAllStores() {
-  theTable.innerHTML = '';
-  createHeader();
-
-  for (var i = 0; i < allStores.length; i++) {
-    allStores[i].render();
-  }
-}
-
-new Store('1st and Pike', 23, 65, 6.3, 'pikePlace');
-new Store('SeaTac Airport', 3, 24, 1.2, 'seatac');
-new Store('Seattle Center', 11, 38, 3.7, 'seacenter');
-new Store('Capitol Hill', 20, 38, 2.3, 'caphill');
-new Store('Alki', 2, 16, 4.6, 'alki');
-
 function createHeader() {
   var trEl = document.createElement('tr');
 
@@ -87,26 +72,93 @@ function createHeader() {
   theTable.appendChild(trEl);
 }
 
+function createFooter() {
+  var hourlyTotal = 0;
+  var totalOfTotals = 0;
+
+  var trEl = document.createElement('tr');
+
+  var thEl = document.createElement('th');
+  thEl.textContent = 'Hourly Totals';
+  trEl.appendChild(thEl);
+
+  for (var i = 0; i < hours.length; i++) {
+    hourlyTotal = 0;
+    for (var j = 0; j < allStores.length; j++) {
+      hourlyTotal += allStores[j].cookiesEachHour[i];
+      totalOfTotals += allStores[j].cookiesEachHour[i];
+    }
+    var thEl = document.createElement('th');
+    thEl.textContent = hourlyTotal;
+    trEl.appendChild(thEl);
+  }
+
+  var thEl = document.createElement('th');
+  thEl.textContent = totalOfTotals;
+  trEl.appendChild(thEl);
+
+  theTable.appendChild(trEl);
+}
+
+function renderAllStores() {
+  theTable.innerHTML = '';
+  createHeader();
+
+  for (var i = 0; i < allStores.length; i++) {
+    allStores[i].render();
+  }
+
+  createFooter();
+}
+
+new Store('1st and Pike', 23, 65, 6.3, 'pikePlace');
+new Store('SeaTac Airport', 3, 24, 1.2, 'seatac');
+new Store('Seattle Center', 11, 38, 3.7, 'seacenter');
+new Store('Capitol Hill', 20, 38, 2.3, 'caphill');
+new Store('Alki', 2, 16, 4.6, 'alki');
+
 //FORM
 function handleSubmit(event) {
   event.preventDefault();
   var location = event.target.loc.value;
-  var min = parseInt(event.target.min.value);
-  var max = parseInt(event.target.max.value);
-  var avg = parseInt(event.target.avg.value);
+  var min = parseFloat(event.target.min.value);
+  var max = parseFloat(event.target.max.value);
+  var avg = parseFloat(event.target.avg.value);
 
   if (!location || !min || !max || !avg) {
     return alert('fields cannot be empty');
   }
+  if (min < 0 || max < 0 || avg < 0 || min > max) {
+    return alert('invalid values');
+  }
+
+  for (var i = 0; i < allStores.length; i++) {
+    if (location === allStores[i].loc) {
+      allStores[i].minCustomers = min;
+      allStores[i].maxCustomers = max;
+      allStores[i].avgCookies = avg;
+      allStores[i].customersEachHour = [];
+      allStores[i].cookiesEachHour = [];
+
+      clearForm();
+
+      renderAllStores();
+      return;
+    }
+  }
 
   new Store(location, min, max, avg);
 
+  clearForm();
+
+  renderAllStores();
+}
+
+function clearForm() {
   event.target.loc.value = null;
   event.target.min.value = null;
   event.target.max.value = null;
   event.target.avg.value = null;
-
-  renderAllStores();
 }
 
 renderAllStores();
